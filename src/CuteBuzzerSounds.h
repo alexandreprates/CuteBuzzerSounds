6da36be5711cefe31c39ceec26354a651494c335
Alexandre Prates <ajfprates@gmail.com>
2023-01-14 16:35:34 -0300

    Update CuteBuzzerSounds.h

--- src/CuteBuzzerSounds.h
+++ src/CuteBuzzerSounds.h
@@ -1,29 +1,20 @@
 #ifndef CuteBuzzerSounds_h
 #define CuteBuzzerSounds_h
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <avr/power.h>
 #include "Sounds.h"
-
-#define debug true
-
 
 class CuteBuzzerSoundsClass
 {
-public:
+  public:
 
-  void init(int buzzerPin);
-  void initBuzzer(int buzzerPin);
+    void init(int buzzerPin);
+    void playTone (float noteFrequency, long noteDuration, int silentDuration, bool isMicro = false);
+    void toneWithVolume(float noteFrequency, long noteDuration, int volume);
+    void bendTones (float initFrequency, float finalFrequency, float prop, long noteDuration, int silentDuration);
+    void playSound(int soundName);
 
-  // Sounds
-  void playTone (float noteFrequency, long noteDuration, int silentDuration, bool isMicro=false);
-  void toneWithVolume(float noteFrequency, long noteDuration, int volume);
-  void bendTones (float initFrequency, float finalFrequency, float prop, long noteDuration, int silentDuration);
-  void playSound(int soundName);
-
-
-private:
-int buzzerPin;
+  private:
+    int buzzerPin;
 };
 
 extern CuteBuzzerSoundsClass cute;
